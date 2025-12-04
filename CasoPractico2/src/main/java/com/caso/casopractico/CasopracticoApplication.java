611546c1457fe91e76bf296f9ff4e305206d3cfb
IssacSegura <issacsegura2005@gmail.com>
2025-12-03 18:58:50 -0600

    commit 1

---
+++ CasoPractico2/src/main/java/com/caso/casopractico/CasopracticoApplication.java
@@ -0,0 +1,13 @@
+package com.caso.casopractico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasopracticoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CasopracticoApplication.class, args);
+	}
+
+}
---
+++ CasoPractico2/src/test/java/com/caso/casopractico/CasopracticoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caso.casopractico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasopracticoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
